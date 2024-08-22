a59c0c016a9dae85faaeb2cf0953a5dfce27eafb
Hodong-Hwang <hodong3470@gmail.com>
2024-08-22 10:13:44 +0900

    Update Timer2.py

--- Timer/Timer2.py
+++ Timer/Timer2.py
@@ -2,22 +2,34 @@ import ntplib
 from datetime import datetime, timedelta
 import time
 import webbrowser
+from concurrent.futures import ThreadPoolExecutor, wait, FIRST_COMPLETED
 
 # NTP 서버 목록
-ntp_servers = ['pool.ntp.org', 'time.google.com', 'time.windows.com']
+#ntp_servers = ['pool.ntp.org', 'time.google.com', 'time.windows.com','time.cloudflare.com']
+ntp_servers = ['time.google.com', 'time.windows.com','time.cloudflare.com']
+
 target_url = "https://sports.cfmc.or.kr/rent/reservation/index/2024/08/13/1/CHEONAN01/19/6"
+client = ntplib.NTPClient()
+
+# NTP를 통해 현재 시간 가져오기 (멀티스레딩과 타임아웃 적용)
+def fetch_ntp_time(server):
+    try:
+        response = client.request(server, version=3, timeout=0.02)  # 타임아웃을 1초로 설정
+        ntp_time = datetime.utcfromtimestamp(response.tx_time)
+        return ntp_time + timedelta(hours=9)  # KST로 변환
+    except (ntplib.NTPException, OSError):
+        return None
 
 # NTP를 통해 현재 시간 가져오기
 def get_ntp_time():
-    client = ntplib.NTPClient()
-    for server in ntp_servers:
-        try:
-            response = client.request(server, version=3)
-            ntp_time = datetime.utcfromtimestamp(response.tx_time)
-            return ntp_time + timedelta(hours=9)  # KST로 변환
-        except ntplib.NTPException as e:
-            print(f"NTPException: Failed to connect to {server}. Trying next server...")
-    raise Exception("Failed to connect to any NTP servers.")
+    with ThreadPoolExecutor(max_workers=len(ntp_servers)) as executor:
+        future_to_server = {executor.submit(fetch_ntp_time, server): server for server in ntp_servers}
+        done, _ = wait(future_to_server.keys(), return_when=FIRST_COMPLETED)  # 가장 먼저 완료된 작업을 기다림
+        for future in done:
+            result = future.result()
+            if result:
+                return result     
+    raise Exception("Failed to connect to any NTP servers.")      
 
 # 특정 시간에 맞춰 URL 열기
 def open_url_at_target_time(target_hour, target_minute, target_second, target_millisecond):
@@ -42,7 +54,5 @@ def open_url_at_target_time(target_hour, target_minute, target_second, target_mi
             print(f"Opened {target_url} at {ntp_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]}")
             break
         # 10밀리초마다 시간 확인
-        time.sleep(0.1)
-
 # 21시 00분 00초 000밀리초(한국 표준시)에 URL 열기 시작
 open_url_at_target_time(20, 59, 59, 750)
